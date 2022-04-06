8cbe75cac8bae52651d8c37526b85fd27463e2d9
Sergey Leschina <mail@putnik.tech>
2022-04-06 23:33:47 +0300

    Set/remove rights for new users

--- commands/welcome.py
+++ commands/welcome.py
@@ -2,6 +2,7 @@
 
 from pyrogram import emoji
 from config import BOT_ID
+from utils.rights import set_rights, remove_rights
 from utils.users import get_username
 
 RULES_URL = "https://telegra.ph/Pravila-chata-russkoj-Vikipedii-10-21"
@@ -16,7 +17,9 @@ MESSAGE = """
 async def welcome(client, message):
     username = get_username(message.from_user.id)
     if username is not None:
+        await set_rights(client, message.from_user.id)
         return
 
+    await remove_rights(client, message.from_user.id)
     text = MESSAGE.format(emoji.WAVING_HAND, RULES_URL, BOT_ID)
     await message.reply_text(text, disable_web_page_preview=True)
