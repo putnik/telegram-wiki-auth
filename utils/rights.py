d2a8a2127dd8d0c3874f07fd8ce03e7fcf484908
Sergey Leschina <mail@putnik.tech>
2022-04-06 22:08:59 +0300

    Change permissions logic

--- utils/rights.py
+++ utils/rights.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from config import CHAT_ID
+from pyrogram.types import ChatPermissions
 
 
 def set_rights(client, tg_id: int):
-    client.promote_chat_member(
-        chat_id=CHAT_ID,
-        user_id=tg_id,
-        can_edit_messages=True,
+    client.restrict_chat_member(CHAT_ID, tg_id, ChatPermissions(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_polls=True,
+        can_add_web_page_previews=True,
         can_invite_users=True,
-    )
+    ))
 
 
 def remove_rights(client, tg_id: int):
-    client.promote_chat_member(
-        chat_id=CHAT_ID,
-        user_id=tg_id,
-        can_edit_messages=False,
+    client.restrict_chat_member(CHAT_ID, tg_id, ChatPermissions(
+        can_send_messages=False,
+        can_send_media_messages=False,
+        can_send_polls=False,
+        can_add_web_page_previews=False,
         can_invite_users=False,
-    )
+    ))
